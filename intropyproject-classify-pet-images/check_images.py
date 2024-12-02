3d37ff10b6e001b17fb7f7fa22b10d876c12ae4a
Afente0808 <132185788+Afente0808@users.noreply.github.com>
2024-12-01 20:57:28 -0800

    Update check_images.py

--- intropyproject-classify-pet-images/check_images.py
+++ intropyproject-classify-pet-images/check_images.py
@@ -41,6 +41,15 @@ from adjust_results4_isadog import adjust_results4_isadog
 from calculates_results_stats import calculates_results_stats
 from print_results import print_results
 
+import argparse
+def get_input_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--dir', type=str, default='pet_images/', help='Directory of pet images')
+    parser.add_argument('--arch', type=str, default='vgg', help='Model architecture')
+    parser.add_argument('--dogfile', type=str, default='dognames.txt', help='Dog names file')
+    return parser.parse_args()
+in_arg = get_input_args()
+
 # Main program function defined below
 def main():
     # TODO 0: Measures total program runtime by collecting start time
